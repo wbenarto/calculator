e0e6fa992f8aa68d7f5afaabf3da8597975d9e03
w.benarto <w.benarto@gmail.com>
2020-04-10 19:52:28 -0700

    Updates

---
+++ precache-manifest.42d3da2e3ee747291f06e6b09f72d11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf29c83717992a7ea233868394c4c39a",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "6272a0a918d6b70c5377",
+    "url": "/calculator/static/css/main.04932c02.chunk.css"
+  },
+  {
+    "revision": "7e6e81811165e8045acf",
+    "url": "/calculator/static/js/2.93baa0b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.93baa0b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6272a0a918d6b70c5377",
+    "url": "/calculator/static/js/main.856037c8.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
